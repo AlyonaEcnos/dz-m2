726d23af9c8ee0e0d5041e6fd898ff554b7958be
Alyona <ecnosss@gmail.com>
2024-02-27 10:54:36 +0200

    v1

---
+++ bot.py
@@ -0,0 +1,243 @@
+from models import Record, AddressBook
+import json
+import os
+
+class InputError(Exception):
+    pass
+
+class DataHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def save_data(self, records):
+        with open(self.file_path, "w") as file:
+            data = {
+                "records": [record.__json__() for record in records]
+            }
+            json.dump(data, file)
+
+    def load_data(self):
+        try:
+            if os.path.getsize(self.file_path) > 0:  
+                with open(self.file_path, "r") as file:
+                    data = json.load(file)
+                    records = [Record(record["name"], record.get("birthday")) for record in data.get("records", [])]
+                    for i, record in enumerate(records):
+                        if "phones" in data.get("records", [])[i]:
+                            for phone in data["records"][i]["phones"]:
+                                record.add_phone(phone)
+                    return records
+        except (OSError, json.JSONDecodeError, KeyError) as e:
+            print(f"Error loading data: {e}")
+            return []
+        
+class FileHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def save_data(self, records):
+        with open(self.file_path, "w") as file:
+            data = {
+                "records": [record.__json__() for record in records]
+            }
+            json.dump(data, file)
+
+    def load_data(self):
+        try:
+            if os.path.getsize(self.file_path) > 0:  
+                with open(self.file_path, "r") as file:
+                    data = json.load(file)
+                    records = [Record(record["name"], record.get("birthday")) for record in data.get("records", [])]
+                    for i, record in enumerate(records):
+                        if "phones" in data.get("records", [])[i]:
+                            for phone in data["records"][i]["phones"]:
+                                record.add_phone(phone)
+                    return records
+        except (OSError, json.JSONDecodeError, KeyError) as e:
+            print(f"Error loading data: {e}")
+            return []
+
+
+class ContactAssistant:
+    def __init__(self, file_handler, address_book):
+        self.file_handler = file_handler
+        self.address_book = address_book
+
+        if os.path.exists(self.file_handler.file_path):
+            self.load_data()
+
+    def load_data(self):
+        records = self.file_handler.load_data()
+        for record in records:
+            self.address_book.add_record(record)
+
+    def save_data(self):
+        self.file_handler.save_data(self.address_book.values())
+
+    def add_contact(self, name, phone, birthday=None):
+        try:
+            record = Record(name, birthday)
+            record.add_phone(str(phone).strip()) 
+            self.address_book.add_record(record)
+            self.save_data()
+            return f"Contact {name} added with phone {phone}"
+        except ValueError as e:
+            raise InputError(str(e))
+
+    def change_contact(self, name, phone):
+        try:
+            record = self.address_book.find(name)
+            if record:
+                record.phones = []  
+                record.add_phone(phone)
+                self.save_data()  
+                return f"Phone number for {name} changed to {phone}"
+            else:
+                raise IndexError
+        except (ValueError, IndexError) as e:
+            raise InputError(str(e))
+        
+    def get_phone(self, name):
+        try:
+            record = self.address_book.find(name)
+            if record:
+                return f"The phone number for {name} is {record.phones[0]}"
+            else:
+                raise IndexError
+        except (ValueError, IndexError) as e:
+            raise InputError(str(e))
+
+    def show_all_contacts(self):
+        records = list(self.address_book.values())
+        if not records:
+            return "No contacts found"
+        else:
+            result = "All contacts:\n"
+            for record in records:
+                phone_numbers = ', '.join(str(phone) for phone in record.phones)
+                result += f"{record.name}: {phone_numbers}\n"
+            return result.strip()
+
+
+class CommandHandler:
+    def __init__(self, contact_assistant):
+        self.contact_assistant = contact_assistant
+
+    def handle_hello(self, args):
+        return "How can I help you?"
+
+    def handle_add(self, args):
+        if len(args) == 0:
+            raise InputError("Invalid command. Please try again. Use 'add <name> <phone>'")
+
+        contact_info = args.split(" ")
+        if len(contact_info) != 3:
+            raise InputError("Invalid command. Please try again. Use 'add <name> <phone>'")
+
+        _, name, phone = args.split(" ")
+        return self.contact_assistant.add_contact(name, phone)
+
+    def handle_change(self, args):
+        if len(args) == 0:
+            raise InputError("Invalid command. Please try again. Use 'change <name> <phone>'")
+
+        contact_info = args.split(" ")
+        if len(contact_info) != 3:
+            raise InputError("Invalid command. Please try again. Use 'add <name> <phone>'")
+
+        _, name, phone = args.split(" ")
+        return self.contact_assistant.change_contact(name, phone)
+
+    def handle_phone(self, args):
+        if len(args) == 0:
+            raise InputError("Invalid command. Please try again. Use 'phone <name>'")
+        args_list = args.split(" ")
+        name = args_list[1]
+        return self.contact_assistant.get_phone(name)
+
+    def handle_show(self, args):
+        return self.contact_assistant.show_all_contacts()
+
+    def handle_bye(self, args):
+        print("Good bye!")
+        return None
+    
+    def handle_search(self, args):
+        if len(args) == 0:
+            raise InputError("Invalid command. Please try again. Use 'search <query>'")
+
+        query = args.split(" ", 1)[1]
+        matching_records = self.contact_assistant.address_book.search(query)
+
+        if not matching_records:
+            return "No matching contacts found"
+        else:
+            result = "Matching contacts:\n"
+            for record in matching_records:
+                phone_numbers = ', '.join(str(phone) for phone in record.phones)
+                result += f"{record.name}: {phone_numbers}\n"
+            return result.strip()
+
+    def choice_action(self, data):
+        actions = {
+            'hello': self.handle_hello,
+            'add': self.handle_add,
+            "change": self.handle_change,
+            "phone": self.handle_phone,
+            'search': self.handle_search,
+            "show": self.handle_show,
+            "close": self.handle_bye,
+            "exit": self.handle_bye,
+            "good bye": self.handle_bye,
+        }
+        return actions.get(data, lambda args: "Invalid command")
+
+    def process_input(self, user_input):
+        try:
+            if not user_input:
+                raise InputError("Invalid command. Please try again.")
+
+            space_index = user_input.find(' ')
+
+            if space_index != -1:
+                first_word = user_input[:space_index]
+            else:
+                first_word = user_input
+
+            if first_word in ["good", "bye"]:
+                first_word = "good bye"
+
+            func = self.choice_action(first_word)
+            result = func(user_input)
+
+            if result is None:
+                return None
+            else:
+                return result
+        except InputError as e:
+            return str(e)
+
+
+def main():
+    file_path = "address_book.json"
+    data_handler = DataHandler(file_path)
+    address_book = AddressBook()
+    contact_assistant = ContactAssistant(data_handler, address_book)
+    command_handler = CommandHandler(contact_assistant)
+
+    while True:
+        try:
+            user_input = input("Enter command: ").lower().strip()
+            result = command_handler.process_input(user_input)
+
+            if result is None:
+                break
+            else:
+                print(result)
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,182 @@
+from datetime import datetime
+from collections import UserDict
+from abc import ABC, abstractmethod
+
+class Field(ABC):
+    def __init__(self, value=""):
+        self.__value = ""
+        self.value = value
+
+    @abstractmethod
+    def is_valid(self, value):
+        pass
+
+    @property
+    @abstractmethod
+    def value(self):
+        pass
+
+    @value.setter
+    @abstractmethod
+    def value(self, new_value):
+        pass
+
+    @abstractmethod
+    def __json__(self):
+        pass
+
+    def __str__(self):
+        return str(self.value)
+
+class StringField(Field):
+    def __init__(self, value=""):
+        super().__init__(value)
+        self.__value = ""
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if not self.is_valid(new_value):
+            raise ValueError(f"Invalid value for {self.__class__.__name__.lower()}")
+        self.__value = new_value
+
+    def __json__(self):
+        return self.value
+
+class DateField(Field):
+    def is_valid(self, value):
+        try:
+            datetime.strptime(value, "%Y-%m-%d")
+            return True
+        except ValueError:
+            return False
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if not self.is_valid(new_value):
+            raise ValueError(f"Invalid value for {self.__class__.__name__.lower()}")
+        self.__value = new_value
+
+    def __json__(self):
+        return self.value
+
+class Name(StringField):
+    def __init__(self, value=""):
+        super().__init__(value)
+
+    def is_valid(self, value):
+        return isinstance(value, str)
+
+    def __json__(self):
+        return self.value 
+
+    def __str__(self):
+        return self.value 
+
+class Phone(StringField):
+    def is_valid(self, value):
+        return isinstance(value, str) and (len(value) == 10 or value.isdigit())
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if not self.is_valid(new_value):
+            raise ValueError(f"Invalid phone number format for '{new_value}'. It should be a 10-digit number.")
+        self.__value = new_value
+
+    def __json__(self):
+        return str(self.value)
+    
+    def __str__(self):
+        return self.value
+
+class RecordSearch:
+    @staticmethod
+    def search_record(record, query):
+        return (
+            isinstance(record.name, StringField) and
+            query.lower() in record.name.value.lower() or
+            any(query in str(phone.value) for phone in record.phones)
+        )
+
+class Record:
+    def __init__(self, name, birthday=None):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = DateField(birthday) if birthday else None
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        initial_len = len(self.phones)
+        self.phones = [p for p in self.phones if p.value != phone]
+        if len(self.phones) == initial_len:
+            raise ValueError(f"Phone number '{phone}' not found")
+
+    def edit_phone(self, old_phone, new_phone):
+        if not Phone(new_phone).is_valid(new_phone):
+            raise ValueError(f"Invalid phone number format for '{new_phone}'")
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                return
+        raise ValueError(f"Phone number '{old_phone}' not found")
+
+    def days_to_birthday(self):
+        if self.birthday:
+            today = datetime.now().date()
+            birthday_date = datetime.strptime(self.birthday.value, "%Y-%m-%d").date()
+
+            next_birthday = datetime(today.year, birthday_date.month, birthday_date.day).date()
+            if today > next_birthday:
+                next_birthday = datetime(today.year + 1, birthday_date.month, birthday_date.day).date()
+
+            days_left = (next_birthday - today).days
+            return days_left
+        else:
+            return None
+
+    def __json__(self):
+        record_data = {
+            "name": self.name.__json__(),
+            "phones": [phone.__json__() for phone in self.phones],
+            "birthday": self.birthday.__json__() if self.birthday else None
+        }
+        return record_data
+
+    def __str__(self):
+        return f"{self.name.__str__()}: {', '.join(str(phone) for phone in self.phones)}"
+
+class AddressBook(UserDict):
+    def __init__(self, *args, **kwargs):
+        self.data = {}
+        super().__init__(*args, **kwargs)
+
+    def search(self, query):
+        return [record for record in self.data.values() if RecordSearch.search_record(record, query)]
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def iterator(self, page_size=10):
+        keys = list(self.data.keys())
+        for i in range(0, len(keys), page_size):
+            yield [self.data[key] for key in keys[i:i + page_size]]
+
+    def find(self, name):
+        return self.data.get(name)
